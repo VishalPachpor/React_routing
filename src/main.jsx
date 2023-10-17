430f220e78f53e788a1df888257151e95f93b40b
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-17 17:05:23 +0530

    Components added

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from '../components/Home/home.jsx'
+import Layout from './Layout.jsx'
+import About from '../components/About/About.jsx'
 
 const router = createBrowserRouter([
-  
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
 ])
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
