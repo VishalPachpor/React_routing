5570e93348244471ac71e24075f86a51e6324a2d
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-18 19:27:22 +0530

    Fetching data from github userName

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,8 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react"
+import { useEffect } from "react"
 
 
 const Github = () => {
-  return (
-    <div>Github</div>
+ const [data, setData] = useState([])
+    useEffect(()=> {
+    fetch('https://api.github.com/users/VishalPachpor')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        setData(data)
+    })
+}, [])
+    return (
+    <div>
+    Github followers : {data.followers}
+    
+    <img src={data.avatar_url} alt="Github Avatar" width={300} />
+    </div>
   )
 }
 
